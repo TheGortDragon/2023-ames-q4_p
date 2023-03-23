25ff84d8d9fd2379085b6c81fd159e1bf288f649
SUPERSONIC\Andre <Andrewhowell921@gmail.com>
2023-03-23 16:32:21 -0700

    Added animation controller, added super basic crouch mechanic

--- Assets/Andrew/Scripts/DefaultMove.cs
+++ Assets/Andrew/Scripts/DefaultMove.cs
@@ -6,11 +6,17 @@ public class DefaultMove : MonoBehaviour {
     // Start is called before the first frame update
     public bool isGround;
     private Rigidbody2D rb2;
+    private SpriteRenderer sr;
+    private Animator animator;
     public float jumpPower;
     public float speed;
 
+    public Sprite CUBEEEE;
+
     void Start() {
         rb2 = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,6 +26,18 @@ public class DefaultMove : MonoBehaviour {
         if (Input.GetButton("Jump") && isGround) {
             rb2.AddForce(new Vector2(0, jumpPower));
         }
+        bool moving = Mathf.Abs(x) > 0;
+        if (moving) {
+            sr.flipX = x < 0;
+        }
+        animator.SetBool("Moving", moving);
+
+        bool crouch = Input.GetButton("Crouch");
+        animator.enabled = !crouch;
+        sr.sprite = CUBEEEE;
+
+        GetComponents<BoxCollider2D>()[0].enabled = !crouch;
+        GetComponents<BoxCollider2D>()[1].enabled = crouch;
     }
 
     public void OnCollisionEnter2D(Collision2D collision) {
